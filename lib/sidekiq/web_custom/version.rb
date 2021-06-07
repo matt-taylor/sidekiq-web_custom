bd3e31c05864492f2d13fd8b1cfa6b9cf8f7bfa7
Matt Taylor <mtaylor@chime.com>
2021-06-07 00:09:24 -0700

    publishing is now working

--- lib/sidekiq/web_custom/version.rb
+++ lib/sidekiq/web_custom/version.rb
@@ -3,8 +3,8 @@
 module Sidekiq
   module WebCustom
     MAJOR = 0  # With backwards incompatability. Requires annoucment and update documentation
-    MINOR = 1  # With feature launch. Documentation of upgrade is useful via a changelog
-    PATCH = 3  # With minor upgrades or patcing a small bug. No changelog necessary
+    MINOR = 2  # With feature launch. Documentation of upgrade is useful via a changelog
+    PATCH = 0  # With minor upgrades or patcing a small bug. No changelog necessary
     VERSION = [MAJOR,MINOR,PATCH].join('.')
 
     def self.get_version
