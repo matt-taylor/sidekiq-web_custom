95347590ed7b5de85812d2c5f8ba714b1f9e36fb
Matt Taylor <mtaylor@chime.com>
2023-08-06 18:32:49 -0700

    version bump

--- lib/sidekiq/web_custom/version.rb
+++ lib/sidekiq/web_custom/version.rb
@@ -3,7 +3,7 @@
 module Sidekiq
   module WebCustom
     MAJOR = 0  # With backwards incompatability. Requires annoucment and update documentation
-    MINOR = 5  # With feature launch. Documentation of upgrade is useful via a changelog
+    MINOR = 6  # With feature launch. Documentation of upgrade is useful via a changelog
     PATCH = 0  # With minor upgrades or patcing a small bug. No changelog necessary
     VERSION = [MAJOR,MINOR,PATCH].join('.')
 
